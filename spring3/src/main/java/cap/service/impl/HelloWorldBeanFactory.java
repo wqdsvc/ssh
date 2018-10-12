9d1d27d75a439bc4df024f43e0ff8423614aa3f9
user <2390241778@qq.com>
2018-10-12 11:30:58 +0800

    Spring Bean的三种初始化

---
+++ spring3/src/main/java/cap/service/impl/HelloWorldBeanFactory.java
@@ -0,0 +1,18 @@
+package cap.service.impl;
+
+import cap.service.HelloWorld;
+
+public class HelloWorldBeanFactory implements HelloWorld {
+    public static HelloWorld createHelloWorldBean() {
+        return new HelloWorldImpl();
+    }
+
+    public HelloWorld createHelloWorldBean2() {
+        return new HelloWorldImpl();
+    }
+
+    @Override
+    public String sayHi(String name) {
+        return null;
+    }
+}
