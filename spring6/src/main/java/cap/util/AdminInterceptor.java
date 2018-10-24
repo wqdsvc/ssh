fb3babc4470260453a8af52b63ec17d0d8ca1101
user <2390241778@qq.com>
2018-10-24 10:08:49 +0800

    Spring AOP的使用

---
+++ spring6/src/main/java/cap/util/AdminInterceptor.java
@@ -0,0 +1,40 @@
+package cap.util;
+
+import net.sf.cglib.proxy.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.AfterReturningAdvice;
+import org.springframework.aop.MethodBeforeAdvice;
+
+import java.lang.reflect.Method;
+import java.util.Date;
+
+public class AdminInterceptor implements MethodBeforeAdvice, AfterReturningAdvice, org.aopalliance.intercept.MethodInterceptor {
+
+
+    @Override
+    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+        String info = methodInvocation.getMethod().getDeclaringClass().getName() + "."
+                + methodInvocation.getMethod().getName() + "()";
+        System.out.println(info);
+        Object result = null;
+        try {
+            result = methodInvocation.proceed();
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("记录中：现在时间是：" + new Date().toString());
+        }
+        return result;
+    }
+
+    @Override
+    public void afterReturning(Object o, Method method, Object[] objects, Object o1) throws Throwable {
+        System.out.println("记录后:现在时间是:" + new Date().toString());
+    }
+
+    @Override
+    public void before(Method method, Object[] objects, Object o) throws Throwable {
+        System.out.println("记录前:现在时间是:" + new Date().toString());
+    }
+}
