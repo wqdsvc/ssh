bc235fc8fdbbedbf2f05ffb2497ae715e9520a8d
user <2390241778@qq.com>
2018-11-11 17:17:24 +0800

    Struts2+Spring+SpringJDBC实现分页显示和添加

---
+++ spring13/src/main/java/cap/util/PageBean.java
@@ -0,0 +1,73 @@
+package cap.util;
+
+import java.util.List;
+
+public class PageBean {
+    private List list;
+    private int pageSize;
+    private int pageNo;
+    private int totalRecords;
+
+    public int getTotalPages() {
+        if (totalRecords % pageSize == 0) {
+            return totalRecords / pageSize;
+        } else {
+            return totalRecords / pageSize + 1;
+        }
+    }
+
+    public int getTopPageNo() {
+        return 1;
+    }
+
+    public int getPreviousPageNo() {
+        if (pageNo <= 1) {
+            return 1;
+        }
+        return pageNo - 1;
+
+    }
+
+    public int getBottomPageNo() {
+        return getTotalPages();
+    }
+
+    public int getNextPageNo() {
+        if (pageNo >= getBottomPageNo())
+            return getBottomPageNo();
+        return pageNo + 1;
+
+    }
+
+    public List getList() {
+        return list;
+    }
+
+    public void setList(List list) {
+        this.list = list;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(int pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public int getTotalRecords() {
+        return totalRecords;
+    }
+
+    public void setTotalRecords(int totalRecords) {
+        this.totalRecords = totalRecords;
+    }
+}
---
+++ spring13/src/main/webapp/error.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user
+  Date: 2018/11/9
+  Time: 10:13
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+error
+</body>
+</html>
