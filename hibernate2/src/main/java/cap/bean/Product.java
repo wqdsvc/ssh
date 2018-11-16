1906b5fd1de54b81638e210cdb63afc58bf75c86
user <2390241778@qq.com>
2018-11-16 11:37:31 +0800

    Hibernate proxool连接池的使用，struts2+spring+hibernate的整合开发

---
+++ hibernate2/src/main/java/cap/bean/Product.java
@@ -0,0 +1,49 @@
+package cap.bean;
+
+public class Product {
+    private int id;
+    private String name;
+    private String serialNumber;
+    private double price;
+    private int stock;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
